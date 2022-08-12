aa4dbdc2deca287c3c30d4535168557dd2d2e256
Daniel Vazquez <dcva9565@hotmail.com>
2022-08-12 17:06:48 -0500

    agregamos express y creamos la primer ruta para que escuche por el puerto 3000

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello Worldddd')
+const express = require('express')
+
+//creado la app
+const app = express()
+
+//routing
+app.get('/', function (req, res) {
+  res.send('Hello World in express')
+})
+
+//Definir un puerto y arrancamos el proyecto
+const port = 3000
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
