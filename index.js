06eebf41789d644d0a38071f5620a29a1dbabce0
Daniel Vazquez <dcva9565@hotmail.com>
2022-08-12 18:52:28 -0500

    instalamos tailwind y configuramos para su funcionamiento, empezamos a agregar clases a pug

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express()
 app.set('view engine', 'pug')
 app.set('views', './views')
 
+//carpeta publica
+app.use(express.static('public'))
+
 //Routing
 app.use('/auth', usuarioRoutes)
 
