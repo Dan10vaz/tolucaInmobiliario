43cb6903f6d3309720a038bc0be988608e146fc0
Daniel Vazquez <dcva9565@hotmail.com>
2022-08-12 18:00:01 -0500

    agregamos pug como template engine para el proyecto y le pasamos la vista a la ruta

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ import express from 'express'
 import usuarioRoutes from './routes/usuarioRoutes.js'
 //Creado la app
 const app = express()
+
+//Habilitar Pug
+app.set('view engine', 'pug')
+app.set('views', './views')
+
 //Routing
-app.use('/', usuarioRoutes)
+app.use('/auth', usuarioRoutes)
 
 //Definir un puerto y arrancamos el proyecto
 const port = 3000
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -2,8 +2,10 @@ import express from 'express'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.json({ msg: 'Hello World in express' })
+router.get('/login', (req, res) => {
+  res.render('auth/login', {
+    autenticado: true,
+  })
 })
 
 export default router
