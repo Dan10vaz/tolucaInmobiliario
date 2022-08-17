10fd5986df1e5bd385b52dedb6b67c8ca1d140e9
Daniel Vazquez <dcva9565@hotmail.com>
2022-08-17 11:32:27 -0500

    consultamos la base de datos de categorias, precios y tipos para despues pasarlos a la vista

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,3 +1,7 @@
+import Categoria from "../models/Categoria.js";
+import Precio from "../models/Precio.js";
+import Tipo from "../models/Tipo.js";
+
 //Renderisa la vista de propiedades de un usuario
 const admin = (req, res) => {
     res.render('propiedades/admin', {
@@ -7,10 +11,20 @@ const admin = (req, res) => {
 }
 
 //Formulario de creación de propiedades
-const crear = (req, res) => {
+const crear = async (req, res) => {
+    //consultar las categorias, precios y los tipos de propiedades
+    const [categorias, precios, tipos] = await Promise.all([
+        Categoria.findAll(),
+        Precio.findAll(),
+        Tipo.findAll()
+    ]);
+
     res.render('propiedades/crear', {
         pagina: 'Crear Propiedad',
-        barra: true
+        barra: true,
+        categorias,
+        precios,
+        tipos
     });
 }
 
