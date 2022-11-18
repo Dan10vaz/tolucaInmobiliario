47426f5ea5199b3027704b30e35b508786c91f6b
Daniel Vazquez <dcva9565@hotmail.com>
2022-11-18 17:39:29 -0600

    terminamos de realizar la lista de propiedades por usuario

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -9,39 +9,63 @@ const admin = async (req, res) => {
 
     const { pagina: paginaActual } = req.query;
 
-    const expresion = /[0-9]/
+    const expresion = /^[1-9]$/
 
     if (!expresion.test(paginaActual)) {
         return res.redirect('/mis-propiedades?pagina=1')
     }
 
-    const { id } = req.usuario;
-    const propiedades = await Propiedad.findAll({
-        where: {
-            usuarioId: id
-        },
-        include: [
-            {
-                model: Imagen,
-                as: 'imagenes'
-            },
-            {
-                model: Categoria,
-                as: 'categoria'
-            },
-            {
-                model: Precio,
-                as: 'precio'
-            }
-        ]
-    })
-    /*  console.log('estas son las propiedades', propiedades) */
+    try {
+        const { id } = req.usuario;
 
-    res.render('propiedades/admin', {
-        pagina: 'Mis Propiedades',
-        propiedades,
-        csrfToken: req.csrfToken(),
-    });
+        //Limites y offset para el paginador
+
+        const limit = 3;
+        const offset = ((paginaActual * limit) - limit);
+
+        const [propiedades, total] = await Promise.all([
+            Propiedad.findAll({
+                limit,
+                offset,
+                where: {
+                    usuarioId: id
+                },
+                include: [
+                    {
+                        model: Imagen,
+                        as: 'imagenes'
+                    },
+                    {
+                        model: Categoria,
+                        as: 'categoria'
+                    },
+                    {
+                        model: Precio,
+                        as: 'precio'
+                    }
+                ],
+            }),
+            Propiedad.count({
+                where: {
+                    usuarioId: id
+                }
+            })
+        ])
+
+        res.render('propiedades/admin', {
+            pagina: 'Mis Propiedades',
+            propiedades,
+            csrfToken: req.csrfToken(),
+            paginas: Math.ceil(total / limit),
+            paginaActual: Number(paginaActual),
+            total,
+            offset,
+            limit,
+        });
+
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 //Formulario de creación de propiedades
